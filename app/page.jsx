1f48a7858d95feef459e225b075ac1c54475c63b
Manas Jain <manasdeepakjain@gmail.com>
2024-08-09 17:32:16 +0530

    First Commit

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col" >
+      <div className="head_text text-center flex-center flex-col">
+        <h1>Discover & Share</h1>
+        <h1 className="orange_gradient"> AI-Powered Prompts</h1>
+      </div>
+    </section>
+  )
+}
+
+export default Home
